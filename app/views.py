5971393a77370110c7bbad2543616505f7d88c33
BJamrowski <bartekjam@interia.eu>
2020-12-16 09:04:36 +0100

    Backend login and register

---
+++ app/views.py
@@ -0,0 +1,18 @@
+from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from .forms import RegisterForm
+
+
+
+def register(response):
+    if response.method == "POST":
+        form = RegisterForm(response.POST)
+        if form.is_valid():
+            form.save()
+        return redirect('/home')
+    else:
+        form = RegisterForm()
+    return render(response, 'register.html', {'form':form})
+
+def home(request):
+    return render(request, "home.html")
