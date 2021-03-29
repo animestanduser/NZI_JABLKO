00d963a8b330c64098961adb9759521263b2f09e
BJamrowski <bartekjam@interia.eu>
2021-03-29 21:17:45 +0200

    Filtrowanie wyswietlania korepetytorow

---
+++ app/models.py
@@ -0,0 +1,110 @@
+from django.db import models
+from django.conf import settings
+from django.utils import timezone
+from django.contrib.auth.models import User
+from django.contrib.auth.models import AbstractUser
+from PIL import Image
+
+
+#Model post
+class Post(models.Model):
+    author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    text = models.TextField()
+    created_date = models.DateTimeField(
+            default=timezone.now)
+    published_date = models.DateTimeField(
+            blank=True, null=True)
+
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save()
+
+    def __str__(self):
+        return self.title
+
+#Model profilu
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    image = models.ImageField(default='none.jpg', upload_to='profile_pics')
+
+    PRZEDMIOTY_CHOICES = [
+    ('brak', 'Brak'),
+    ('historia', 'Historia'),
+    ('angielski', 'Angielski'),
+    ('informatyka', 'Informatyka'),
+    ('matematyka', 'Matematyka'),
+    ('polski', 'Polski'),
+    ]
+
+    MIEJSCOWOSC_CHOICES = [
+    ('brak', 'Brak'),
+    ('warszawa', 'Warszawa'),
+    ('gdynia', 'Gdynia'),
+    ('kraków', 'Kraków'),
+    ('poznań', 'Poznań'),
+    ('wrocław', 'Wrocław'),
+    ('ciechanów', 'Ciechanów'),
+    ]
+
+    przedmiot = models.CharField(max_length=30, choices=PRZEDMIOTY_CHOICES, default='brak')
+    miejscowosc = models.CharField(max_length=30, choices=MIEJSCOWOSC_CHOICES, default='brak')
+    cena = models.CharField(max_length=400)
+    opis = models.TextField(max_length="500")
+    korepetytor = models.BooleanField(default=False)
+    users = User.objects.all()
+    args = {'users':users,}
+
+    def __str__(self):
+        return f'{self.user.username} - Profil'
+        
+
+
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+
+        img = Image.open(self.image.path)
+
+        if img.height > 300 or img.width > 300:
+            output_size = (300,300)
+            img.thumbnail(output_size)
+            img.save(self.image.path)
+
+
+
+    
+    cena = models.CharField(max_length=400)
+
+
+    opis = models.TextField(max_length="500")
+
+    
+
+
+#Model zgłoszeń użytkowników
+class Report(models.Model):
+    user_author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='user_author')
+    user_reported = models.OneToOneField(User, on_delete=models.CASCADE, related_name='user_reported')
+    message = models.TextField()
+
+    def __str__(self):
+        return f'{self.user_author.username} ->  {self.user_reported.username}'
+
+    
+
+    
+
+
+#Model wiadomości
+class Message(models.Model):
+    sender = models.ForeignKey(User, on_delete=models.CASCADE, related_name='sender')
+    receiver = models.ForeignKey(User, on_delete=models.CASCADE, related_name='receiver')
+    message = models.CharField(max_length=1200)
+    timestamp = models.DateTimeField(auto_now_add=True)
+    is_read = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.message
+
+    class Meta:
+        ordering = ('timestamp',)
---
+++ app/urls.py
@@ -0,0 +1,66 @@
+from django.urls import path, include
+from . import views
+from django.conf.urls import url
+from django.contrib.auth import views as auth_views
+from django.conf import settings
+from django.conf.urls.static import static 
+from django.conf import settings
+
+urlpatterns = [
+
+    #strona główna(muszą być oba bo inaczej error)
+    path('', views.start, name='home'),
+    path('', views.start, name='index'),
+
+    #rejestracja i przypominanie hasła
+    url(r'^signup/$', views.signup, name='signup'),
+    url(r'^activate/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
+        views.activate, name='activate'),
+
+    url(r'^password_change/$', views.change_password, name='change_password'),
+    
+
+
+    url(r'^accounts/password_reset/$', auth_views.PasswordResetView.as_view(template_name = "registration/password_reset.html"),name ='reset_password'),
+
+
+    url(r'^accounts/password_reset/done/$', auth_views.PasswordResetDoneView.as_view(template_name = "registration/password_reset_sent.html"), name ='password_reset_done'),
+    
+    url(r'^accounts/reset/done/$', auth_views.PasswordResetCompleteView.as_view(template_name = "registration/password_reset_done.html"), name ='password_reset_complete'),
+
+ 
+    path('reset/<uidb64>/<token>', auth_views.PasswordResetConfirmView.as_view(template_name = "registration/password_reset_form.html"), name ='password_reset_confirm'),
+ 
+
+  
+
+    #Podgląd profilu
+    path('profile/<int:user>/', views.profile, name='profile'),
+    
+
+    #Szukaj korepetytorów
+    path('search/', views.search, name='search'),
+
+    #Lista korepetytorów
+    path('list/', views.list, name='list'),
+
+    #Chybił trafił
+    path('random/', views.random, name='random'),
+
+    #Edycja przedmiotów oraz miejscowości
+    path('edit_personal/', views.edit_personal, name='edit_personal'),
+
+    #Edycja danych osobowych
+    path('edit/', views.edit, name='edit'),
+
+    #Report profilu
+    path('report/<int:user>/', views.report, name='report'),
+    
+  
+    path('chat/<int:sender>/<int:receiver>/', views.message_view, name='chat'),
+
+    path('api/messages/<int:sender>/<int:receiver>/', views.message_list, name='message-detail'),
+    path('api/messages/', views.message_list, name='message-list'),
+]
+if settings.DEBUG:
+        urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
